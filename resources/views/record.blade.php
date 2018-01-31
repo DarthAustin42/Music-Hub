6e4e73109dda475978c52e74e7695d6a4eaa159f
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-31 12:35:31 -0600

    Change style of table

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -1,12 +1,42 @@
 <html>
-<body>
-	@foreach($record as $records)
-	    <p class="title">{{$records->title}}</p>
-	    <p class="author">{{$records->author}}</p>
-	    <p class="album">{{$records->album}}</p>
-	    <p class="genre">{{$records->genre}}</p>
-	    <p class="time">{{$records->time}}</p>
-	@endforeach
-</body>
+	<head>
+		<style>
+			table {
+			    font-family: arial, sans-serif;
+			    border-collapse: collapse;
+			    width: 100%;
+			}
+
+			td, th {
+			    border: 1px solid #dddddd;
+			    text-align: left;
+			    padding: 8px;
+			}
+
+			tr:nth-child(even) {
+			    background-color: #dddddd;
+			}
+		</style>
+	</head>
+	<body>
+		<table>
+		  	<tr>
+		    	<th>Title</th>
+			    <th>Author</th>
+			    <th>Album</th>
+			    <th>Genre</th>
+			    <th>Time</th>
+		  	</tr>
+		  	@foreach($record as $records)
+			  	<tr>
+				    <td>{{$records->title}}</td>
+				    <td>{{$records->author}}</td>
+				    <td>{{$records->album}}</td>
+				    <td>{{$records->genre}}</td>
+				    <td>{{$records->time}}</td>
+				</tr>
+			@endforeach
+		</table>
+	</body>
 </html>
 
