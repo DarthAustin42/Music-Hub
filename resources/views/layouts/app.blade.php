cb51ed3f83dd7a5ac83522648738de500c395d73
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-31 14:02:57 -0600

    Chaning Adding Style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,15 @@
                 padding: 10px;
                 padding-bottom: 0px;
             }
+            input[type=text], select {
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+            }
         </style>
         <!-- CSS And JavaScript -->
     </head>
