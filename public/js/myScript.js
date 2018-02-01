86837496881fd5519e8ffa7656d267d034762f84
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-31 23:11:58 -0600

    Adding Edit

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -3,7 +3,8 @@ function changeRecord() {
     document.getElementById("recordList").selectedIndex = x;
 
 	var y;
-	console.log(document.getElementById("songList").rows.length);
+	console.log(document.getElementById("songList").rows[y].cells[0].innerHTML);
+	console.log(document.getElementById("recordListTitle").text);
     for(y = 0; y < document.getElementById("songList").rows.length; y++) {
     	if(document.getElementById("songList").rows[y].cells[0].innerHTML === document.getElementById("recordListTitle").text) {
     		document.getElementById("record-title2").value = document.getElementById("songList").rows[y].cells[0].innerHTML;
