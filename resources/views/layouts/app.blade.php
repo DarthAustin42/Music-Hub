badd68ff1be27e2ae4a8f0831e7e97cbc0f8dd85
Austin Nielsen <austinpnielsen@hotmail.com>
2018-02-01 12:58:38 -0600

    Changing Styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <body>
     	<div class="container">
 		  <div class="jumbotron">
-		    <center><h1>Music Hub</h1></center>
+		    <center><h1 style="font-family: 'Raleway', sans-serif;">Music Hub</h1></center>
 		  </div>
 		</div>
         @yield('currentRecords')
