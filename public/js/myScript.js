5b1b86e807d9ec15b506fc01e804cb2011f4f7a9
Austin Nielsen <austinpnielsen@hotmail.com>
2018-01-31 22:55:07 -0600

    Adding Edit

---
+++ public/js/myScript.js
@@ -0,0 +1,4 @@
+function changeRecord() {
+    var x = document.getElementById("recordListTitle").selectedIndex;
+    document.getElementById("recordList").selectedIndex = x;
+}
--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -83,11 +83,18 @@
 		            {{ csrf_field() }}
 
 		            <div class="form-group">
-		            	<select name="recordList" class="selectpicker">
-		            		@foreach($record as $records)
-		            			<option>{{$records->id}}</option>
-		            		@endforeach
-		            	</select>
+		            	<center>
+		            		<select id="recordListTitle" name="recordListTitle" onchange="changeRecord()" class="selectpicker">
+			            		@foreach($record as $records)
+			            			<option>{{$records->title}}</option>
+			            		@endforeach
+			            	</select>
+			            	<select id="recordList" name="recordList" class="selectpicker" hidden="true">
+			            		@foreach($record as $records)
+			            			<option>{{$records->id}}</option>
+			            		@endforeach
+			            	</select>
+			            </center>
 		            	<br>
 		            	<table>
 						  	<tr><input type="text" name="title" id="record-title" class="form-control" placeholder="Title"></tr>
@@ -107,6 +114,7 @@
 		    </div>
 	    @endsection
 	</body>
+	<script src="/js/myScript.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
