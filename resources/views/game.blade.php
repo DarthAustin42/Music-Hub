d768381437c181823f536e3792fb192f0ce3898d
Austin Nielsen <austinpnielsen@hotmail.com>
2018-05-17 11:28:40 -0500

    Adding Cards

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -51,14 +51,14 @@
 			    		<div class='col'></div>
 			    		<div class='col'>
 			    			<div class='row'>
-			    				<div class='col'>{{$games->card1}}</div>
 			    				<div class='col'><img src='img/{{ $games->card1 }}.svg'></div>
-			    				<div class='col'>abc42</div>
+			    				<div class='col'><img src='img/{{ $games->card2 }}.svg'></div>
+			    				<div class='col'><img src='img/{{ $games->card3 }}.svg'></div>
 			    			</div>
 			    			<div class='row'>
-			    				<div class='col'>abc42</div>
-			    				<div class='col'>abc42</div>
-			    				<div class='col'>abc42</div>
+			    				<div class='col'><img src='img/{{ $games->card4 }}.svg'></div>
+			    				<div class='col'><img src='img/{{ $games->card5 }}.svg'></div>
+			    				<div class='col'><img src='img/{{ $games->card6 }}.svg'></div>
 			    			</div>
 			    		</div>
 			    		<div class='col'></div>
