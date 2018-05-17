495dd484f0bffedc0c4196c6de7033c2bcf8ae4c
Austin Nielsen <austinpnielsen@hotmail.com>
2018-05-17 09:49:17 -0500

    Changes to blade.php

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,75 +6,78 @@
 	<body>
 
 		@extends('layouts.app')
-	    <div class="container gameBoard">
-	        <div class="row grow">
-	            <div class="col-xs-4 gcol"></div>
-	            <div class="col-xs-4 gcol Player">
-	                <div class="container cardsHolder">
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card11"></div>
-	                        <div class="col-xs-4 cards" id="card12"></div>
-	                        <div class="col-xs-4 cards" id="card13"></div>
-	                    </div>
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card14"></div>
-	                        <div class="col-xs-4 cards" id="card15"></div>
-	                        <div class="col-xs-4 cards" id="card16"></div>
-	                    </div>
-	                </div>
-	            </div>
-	            <div class="col-xs-4 gcol"></div>
-	        </div>
-	        <div class="row grow">
-	            <div class="col-xs-4 gcol Player">
-	                <div class="container cardsHolder">
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card21"></div>
-	                        <div class="col-xs-4 cards" id="card22"></div>
-	                        <div class="col-xs-4 cards" id="card23"></div>
-	                    </div>
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card24"></div>
-	                        <div class="col-xs-4 cards" id="card25"></div>
-	                        <div class="col-xs-4 cards" id="card26"></div>
-	                    </div>
-	                </div>
-	            </div>
-	            <div class="col-xs-4 gcol"></div>
-	            <div class="col-xs-4 gcol Player">
-	                <div class="container cardsHolder">
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card31"></div>
-	                        <div class="col-xs-4 cards" id="card32"></div>
-	                        <div class="col-xs-4 cards" id="card33"></div>
-	                    </div>
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card34"></div>
-	                        <div class="col-xs-4 cards" id="card35"></div>
-	                        <div class="col-xs-4 cards" id="card36"></div>
-	                    </div>
-	                </div>
-	            </div>
-	        </div>
-	        <div class="row grow">
-	            <div class="col-xs-4 gcol"></div>
-	            <div class="col-xs-4 gcol Player">
-	                <div class="container cardsHolder">
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card41"></div>
-	                        <div class="col-xs-4 cards" id="card42"></div>
-	                        <div class="col-xs-4 cards" id="card43"></div>
-	                    </div>
-	                    <div class="row">
-	                        <div class="col-xs-4 cards" id="card44"></div>
-	                        <div class="col-xs-4 cards" id="card45"></div>
-	                        <div class="col-xs-4 cards" id="card46"></div>
-	                    </div>
-	                </div>
-	            </div>
-	            <div class="col-xs-4 gcol"></div>
-	        </div>
-	    </div>		
+
+		@section('currentRecords')
+		    <div class="container gameBoard">
+		        <div class="row grow">
+		            <div class="col-xs-4 gcol"></div>
+		            <div class="col-xs-4 gcol Player">
+		                <div class="container cardsHolder">
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card11"></div>
+		                        <div class="col-xs-4 cards" id="card12"></div>
+		                        <div class="col-xs-4 cards" id="card13"></div>
+		                    </div>
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card14"></div>
+		                        <div class="col-xs-4 cards" id="card15"></div>
+		                        <div class="col-xs-4 cards" id="card16"></div>
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="col-xs-4 gcol"></div>
+		        </div>
+		        <div class="row grow">
+		            <div class="col-xs-4 gcol Player">
+		                <div class="container cardsHolder">
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card21"></div>
+		                        <div class="col-xs-4 cards" id="card22"></div>
+		                        <div class="col-xs-4 cards" id="card23"></div>
+		                    </div>
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card24"></div>
+		                        <div class="col-xs-4 cards" id="card25"></div>
+		                        <div class="col-xs-4 cards" id="card26"></div>
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="col-xs-4 gcol"></div>
+		            <div class="col-xs-4 gcol Player">
+		                <div class="container cardsHolder">
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card31"></div>
+		                        <div class="col-xs-4 cards" id="card32"></div>
+		                        <div class="col-xs-4 cards" id="card33"></div>
+		                    </div>
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card34"></div>
+		                        <div class="col-xs-4 cards" id="card35"></div>
+		                        <div class="col-xs-4 cards" id="card36"></div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="row grow">
+		            <div class="col-xs-4 gcol"></div>
+		            <div class="col-xs-4 gcol Player">
+		                <div class="container cardsHolder">
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card41"></div>
+		                        <div class="col-xs-4 cards" id="card42"></div>
+		                        <div class="col-xs-4 cards" id="card43"></div>
+		                    </div>
+		                    <div class="row">
+		                        <div class="col-xs-4 cards" id="card44"></div>
+		                        <div class="col-xs-4 cards" id="card45"></div>
+		                        <div class="col-xs-4 cards" id="card46"></div>
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="col-xs-4 gcol"></div>
+		        </div>
+		    </div>
+	    @endsection	
 	</body>
 	<script src="/js/myScript.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
