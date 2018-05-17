3b8d00b33cc6092b3614df98e928342d036eb546
Austin Nielsen <austinpnielsen@hotmail.com>
2018-05-17 11:08:22 -0500

    Adding Cards

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -51,7 +51,7 @@
 			    		<div class='col'>
 			    			<div class='row'>
 			    				<div class='col'></div>
-			    				<div class='col'><img src={{ URL::asset("img/{{$games->card1}}") }}></div>
+			    				<div class='col'><img src={{ URL::asset("img/{{$games->card1}}")) }}></div>
 			    				<div class='col'>abc42</div>
 			    			</div>
 			    			<div class='row'>
